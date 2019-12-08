ff51f42456fa2e03c6a8ca2ff71a9ab138a1d90b
arishaked <arishaked10@gmail.com>
2019-12-08 19:10:07 +0200

    building_burger_module

--- src/components/OrderSummary/OrderSummary.js
+++ src/components/OrderSummary/OrderSummary.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-import Auxillary from './../../hoc/Auxillary';
+import Auxillary from '../../hoc/Auxillary/Auxillary';
 import Button from './../UI/Button/Button';
 
-const orderSummary = props => {
+const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients).map(ingrKey => {
     return (
       <li key={ingrKey}>
-        <span style={{ textTransform: 'capitalize' }}>{ingrKey}</span>: {props.ingredients[ingrKey]}
+        <span style={{ textTransform: 'capitalize' }}>{ingrKey}</span>:{' '}
+        {props.ingredients[ingrKey]}
       </li>
     );
   });
@@ -16,10 +17,16 @@ const orderSummary = props => {
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}</strong>
+      </p>
       <p>Continue to Checkout?</p>
-      <Button btnType={"Danger"} clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button btnType={"Success"} clicked={props.purchaseContinued}>CONTINUE</Button>
+      <Button btnType={'Danger'} clicked={props.purchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType={'Success'} clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
     </Auxillary>
   );
 };
